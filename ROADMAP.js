39f66a2d1b2b817bd808deffcce01764826b67a5
Robert0101 <rob.tangled@gmail.com>
2022-08-12 18:31:46 -0300

    Crear ROADMAP. Crear .env y agregar la MY_API_KEY

---
+++ ROADMAP.js
@@ -0,0 +1,13 @@
+// API Key de Spoontacular:
+// 08627e517b4943fe9b66893317a91541
+
+//?En api crear un archivo llamado: .env que tenga la siguiente forma:
+// DB_USER=usuariodepostgres
+// DB_PASSWORD=passwordDePostgres
+// DB_HOST=localhost
+// Reemplazar usuariodepostgres y passwordDePostgres con tus propias credenciales para conectarte a postgres. Este archivo va ser ignorado en la subida a github, ya que contiene información sensible (las credenciales).
+//?-----
+
+//? Crear desde psql una base de datos llamada food.
+
+// El contenido de client fue creado usando: Create React App.
