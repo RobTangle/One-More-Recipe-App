a48a50c345a0707cf534eea450aca152343800a9
Robert0101 <rob.tangled@gmail.com>
2022-08-13 20:17:09 -0300

    Commit igual al anterior?

--- ROADMAP.js
+++ ROADMAP.js
@@ -15,7 +15,22 @@ const server = require("./api/src/app");
 // El contenido de client fue creado usando: Create React App.
 
 //*Pensar bien la estructura de cada app, pensar el rol de cada componente, micro y macro, Detallar y escribir los detalles de cada parte de la app. Esto me va a ayudar a entender bien todo.
-//h 1) Crear los models.
+
+//h 1) Crear los models.:
+// Base de datos
+// El modelo de la base de datos deberá tener las siguientes entidades (Aquellas propiedades marcadas con asterisco deben ser obligatorias):
+//?Tener presente que no se tienen que mezclar los id de mis instancias en la BD con la de la API de spoontacular.
+//  Receta con las siguientes propiedades:
+// ID: *
+// Nombre *
+// Resumen del plato *
+// Nivel de "comida saludable" (health score)
+// Paso a paso
+
+//  Tipo de dieta con las siguientes propiedades:
+// ID
+// Nombre
+// La relación entre ambas entidades debe ser de muchos a muchos ya que una receta puede ser parte de varios tipos de dieta en simultaneo y, a su vez, un tipo de dieta puede contener múltiples recetas distintas.
 
 //h 2) Crear rutas. Cada ruta con su propio archivo adentro de /routes.
 //h El archivo app.js importa el objeto de rutas y lo usa  como middleware o algo así, con un server.use(router)
