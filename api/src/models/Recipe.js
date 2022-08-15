ecca146bad9a04e7ae0ff735956bb8d942a6db65
Robert0101 <rob.tangled@gmail.com>
2022-08-15 14:17:48 -0300

    Completar Model Recipe. Falta el tema del ID

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,27 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  //h Voy a tener que hacer que el id de estas recetas sea distinta a las id de la API.
+  sequelize.define("recipe", {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    healthScore: {
+      type: DataTypes.FLOAT,
+      validate: {
+        min: 0,
+        max: 10,
+      },
+    },
+    steps: {
+      type: DataTypes.TEXT,
     },
   });
 };
