81bc5355ce4c9de5295e27b7ebeb61a01a38d7f2
Robert0101 <rob.tangled@gmail.com>
2022-08-15 20:10:01 -0300

    Agregar middlewares recetaRouter y dietsRouter. Crear rutas de prueba

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,33 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const router = Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
-const router = Router();
+const recetaRouter = require("./recipes");
+const dietsRouter = require("./diets");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use("/recipes", recetaRouter);
+router.use("/diets", dietsRouter);
 
+//!---- rutas de prueba: ------
+// router.get("/", async (req, res) => {
+//   res.status(255).send({ hola: "Hello!!!!" });
+// });
+
+router.get("/", async (req, res) => {
+  const { name } = req.query;
+});
+
+router.post("/postear", async (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    return res
+      .status(400)
+      .send(`name or description missing: ${name} y ${description}`);
+  }
+  return res.status(399).send(`OK. Recibidos ${name} y ${description}`);
+});
+//!-----------------------------
 
 module.exports = router;
