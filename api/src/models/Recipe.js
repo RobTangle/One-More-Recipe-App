ecdf92019baa4af08733322238f2a31f6aa56a7e
Robert0101 <rob.tangled@gmail.com>
2022-08-15 23:12:59 -0300

    Ideas sobre propiedad diet.

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -23,5 +23,14 @@ module.exports = (sequelize) => {
     steps: {
       type: DataTypes.TEXT,
     },
+    // diet: {}
+    //Tengo que usar ese método tipo addModels() para asociar dietas a una receta? Entra como argumento a ese método un array de PKs. Y los PK tienen que ser los de las dietas!
+    //Le digo onda: si dietArray.length > 0, entonces pasá los PK de ese array como argumento del método que tengo que usar para asociar!! Creo que re va!!!!
+    // podría recibir un número que represente el id de la dieta en cuestión,
+    //Al crear las relaciones con los Recipe.hasMany(Diet);  Diet.hasMany(Recipe); se crean automáticamente unos métodos que son para relacionar las dos: set/addModels()
+    //Cuando me lleguen los datos por el req.body, chequeo si me enviaron las PK.
+    // o un array de números?
+    // después podría hacer un filtrado de todas las recetas que tienen en su propiedad diet ciertos números, y así filtrar las recetas según sus dietas. (Reviso el array y listo)
+    //podría recibir los nombres de las dietas en una frase separada por espacios?
   });
 };
