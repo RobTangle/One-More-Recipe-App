0ec7b9d3a481f73f81ba969cd4b4f2a66d945961
Robert0101 <rob.tangled@gmail.com>
2022-08-17 14:15:36 -0300

    Agregar componente para empezar a probar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import { Route } from "react-router-dom";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Food</h1>
+      <h3>Movie Detail:</h3>
+      <Route path="/recipes/:id" component={RecipeDetail} />
     </div>
   );
 }
