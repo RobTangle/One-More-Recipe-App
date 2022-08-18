859755619ef5b06c6d78e817c8f20a962293f432
Robert0101 <rob.tangled@gmail.com>
2022-08-17 23:57:53 -0300

    Agregar Routes para renderizar más componentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { Route } from "react-router-dom";
-import RecipeDetail from "./components/RecipeDetail";
+import RecipeDetail from "./components/RecipeDetail/RecipeDetail.jsx";
+import Nav from "./components/Nav/Nav.jsx";
+import createRecipe from "./components/createRecipe/createRecipe";
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Food</h1>
-      <h3>Movie Detail:</h3>
+      <Route path="/" component={Nav} />
+      <h3>RECIPES!</h3>
       <Route path="/recipes/:id" component={RecipeDetail} />
+      <Route exact path="/createRecipe/" component={createRecipe} />
     </div>
   );
 }
