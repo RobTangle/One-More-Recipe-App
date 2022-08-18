9b8fba823d4dc004a7f207b6530620fef92e4c16
Robert0101 <rob.tangled@gmail.com>
2022-08-18 18:55:15 -0300

    Importar componentes. Crear nuevas Routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,20 @@ import RecipeDetail from "./components/RecipeDetail/RecipeDetail.jsx";
 import Nav from "./components/Nav/Nav.jsx";
 import createRecipe from "./components/createRecipe/CreateRecipe";
 import DBRecipes from "./components/DBRecipes/DBRecipes";
+import LandingPage from "./components/LandingPage/LandingPage";
+import SearchRecipes from "./components/SearchRecipes/SearchRecipes";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
-      <h3>RECIPES!</h3>
-      <Route path="/" component={Nav} />
-      <Route path="/recipes/:id" component={RecipeDetail} />
-      <Route exact path="/createRecipe/" component={createRecipe} />
-      <Route exact path="/recipesInDataBase/" component={DBRecipes} />
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/home" component={Nav} />
+      <Route path="/home" component={SearchRecipes} />
+      <Route path="/home/recipes/:id" component={RecipeDetail} />
+      <Route path="/home/searchRecipes" component={SearchRecipes} />
+
+      <Route exact path="/home/createRecipe/" component={createRecipe} />
+      <Route exact path="/home/recipesInDataBase/" component={DBRecipes} />
     </div>
   );
 }
