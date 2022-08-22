9319aaf5768401917c813eae373a61f2e4711610
Robert0101 <rob.tangled@gmail.com>
2022-08-22 01:56:49 -0300

    Meter muchos checkboxes. Queda mucho por hacer acá

--- client/src/components/createRecipe/createRecipe.jsx
+++ client/src/components/createRecipe/createRecipe.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import * as actions from "../../redux/actions/index";
+import "./createRecipe.css";
 // Importar las actions como Object Modules, sino los test no funcionarán!
 
 //  Un formulario controlado con JavaScript con los siguientes campos:
@@ -38,12 +39,22 @@ const CreateRecipe = () => {
           <legend>Create Recipe: </legend>
           <div className="createRecipe">
             <div>
-              <label htmlFor="">Title: </label>
-              <input type="text" name="title" onChange={handleOnChange} />
+              <label htmlFor="title">Title * </label>
+              <input
+                required
+                type="text"
+                id="title"
+                name="title"
+                onChange={handleOnChange}
+              />
             </div>
             <div>
-              <label htmlFor="">Health score: </label>
+              <label htmlFor="health-score">Health score * </label>
               <input
+                required
+                min="0"
+                max="100"
+                id="health-score"
                 type="number"
                 name="healthScore"
                 onChange={handleOnChange}
@@ -51,17 +62,20 @@ const CreateRecipe = () => {
             </div>
             {/* hacer validaciones de min=0 y max=100 */}
             <div>
-              <label htmlFor="">Summary: </label>
+              <label htmlFor="summary">Summary * </label>
               <textarea
+                required
                 name="summary"
-                id=""
+                minLength="1"
+                id="summary"
+                placeholder="Don't introduce simbols like '[ ] { } > < '"
                 cols="30"
                 rows="10"
                 onChange={handleOnChange}
               ></textarea>
             </div>
             <div>
-              <label>Step by step: </label>
+              <label htmlFor="steps">Step by step </label>
               <textarea
                 name="steps"
                 id="steps"
@@ -71,7 +85,7 @@ const CreateRecipe = () => {
               ></textarea>
             </div>
             <div>
-              <label htmlFor="">diet: </label>
+              <label htmlFor="">diet </label>
               <input type="text" name="diet" onChange={handleOnChange} />
             </div>
             <div>
@@ -108,6 +122,125 @@ const CreateRecipe = () => {
                 name="carnivore"
                 id="carnivore_checkbox"
                 value="carnivore"
+              />
+            </div>
+            <div>
+              <label htmlFor="">Gluten Free</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Gluten Free"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Ketogenic</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Ketogenic"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Vegeterian</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Vegeterian"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Lacto-Vegeterian</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Lacto-vegeterian"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Ovo-Vegeterian</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Ovo-Vegeterian"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Vegan</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Vegan"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Pescetarian</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Pescetarian"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Paleo</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Paleo"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Primal</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Primal"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Low FODMAP</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Low FODMAP"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Whole30</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Whole30"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Omnivore</label>
+              <input
+                type="checkbox"
+                name="carnivore"
+                id="carnivore_checkbox"
+                value="Omnivore"
               />
             </div>
             <button type="submit">Create Recipe</button>
