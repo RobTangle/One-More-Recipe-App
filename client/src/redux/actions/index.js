5c2835112f256a76b476a997b879590702ef2670
Robert0101 <rob.tangled@gmail.com>
2022-08-23 22:49:39 -0300

    Crear action clearDetail

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -6,6 +6,7 @@ export const GET_RECIPE_DETAIL = "GET_RECIPE_DETAIL";
 export const DELETE_RECIPE = "DELETE_RECIPE";
 export const CREATE_RECIPE = "CREATE_RECIPE";
 export const GET_ALL_FROM_DB = "GET_ALL_FROM_DB";
+export const CLEAR_DETAIL = "CLEAR_DETAIL";
 
 // export const getRecipeDetail = (id) => {
 //   return async function (dispatch) {
@@ -109,6 +110,16 @@ export const sortByTitle = () => {
   };
 };
 //!----------------------------------------
+
+export const clearDetail = () => {
+  return function (dispatch) {
+    try {
+      return dispatch({ type: CLEAR_DETAIL, payload: "" });
+    } catch (error) {
+      return error.message;
+    }
+  };
+};
 
 // export const getRecipeDetailFIXED = (id) => {
 //   return async function (dispatch) {
