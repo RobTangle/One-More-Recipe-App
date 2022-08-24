34e63917323918b56f7c940cf1e84d0149a55125
Robert0101 <rob.tangled@gmail.com>
2022-08-23 21:19:56 -0300

    Limpiar un poco los comentarios

--- ROADMAP.js
+++ ROADMAP.js
@@ -1,6 +1,5 @@
 // API Key de Spoontacular:
 // 08627e517b4943fe9b66893317a91541
-// const server = require("./api/src/app");  Y ESTO POR QUÉ LO TENGO ACÁ? JAJAJ
 
 //* Enunciado
 // La idea general es crear una aplicación en la cual se puedan ver distintas recetas de comida junto con información relevante de las mismas utilizando la api externa spoonacular y a partir de ella poder, entre otras cosas:
@@ -25,17 +24,10 @@
 //! PROBLEMAS DETECTADOS PARA SOLUCIONAR:
 //! * Home/ Cuando busco con By title: Search. me devuelve .... TODO LO DE LA DB y además resultados de API.
 //! * Home/ Cuando busco con "Search title and diet" me devuelve... sólo resultados de la API.
-//! Podría hacer un slice de State.recipes y renderizar de a tramos de (i, i+9) por ejemplo...
-//! * Order by title no funciona. Sólo el Invert funciona.
-//! * Si no hay recetas para renderizar, renderizar un mensaje.
-//! * Order by Health score INVERT funciona bien. Pero "Order by Health score" está trabajando sobre el store.recipes y no sobre el localState.
 //! Cuando ya hay una Recipe detail cargada, tarda mucho en renderizarmela
 //H ITINERARIO: ----------------------------------------------------------------------------------
-//h OK Sacar los botones de buscado por title y receta individualmente y dejar uno solo.
-//h Poner un botón para eliminar filter por dieta, y que simplemente renderice todo el store.recipes.
-//h * Filtrar por tipo de dieta. Le hacer un filter al State.Recipes según si el array de diets includes "dieta";
-//h * Hacer que el get me traiga 99 recetas. Y hacer que sólo renderice las primeras 9 por cada página. Pensar bien cómo hacer esto... splices? slices? contador en el mapeo?
-//h * Pensar cómo establecer la cantidad de páginas que hay. Si está apretado el botón 2, podría decir que me renderice del 10 al 19, por ejemplo. Y que se creen botones según la cantidad de elementos haya en el array. State.recipes.length / 9;
+
+//h * Hacer que el get me traiga 99 recetas.
 //? IDEAS: -----------------------------------------------------------------------------------------
 //? 1* Para agregar las dietas podría usar el método add que me brinda sequelize cuando hago asociaciones. Le paso un array de Pks de dietas? Podría agregar esas PK a un arreglo y enviarlo en el post.
 //? 2* Podría hacer que las recetas que me traigo de la DB vayan a un State.DBRecipes y que esas recetas se rendericen a su propia manera ya que no tienen imagen, etc.
@@ -44,6 +36,8 @@
 //? RANDOM:
 
 //?--------------------------------------------------------
+
+//* https://stackoverflow.com/questions/56926282/react-hooks-fetch-wont-stop-fetching
 
 //FUNCIÓN PARA PASAR LOS QUERY A STRING QUE VA A IR A LA URL:
 let query = {
