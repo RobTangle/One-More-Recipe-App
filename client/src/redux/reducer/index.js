d3c829ffe07fc476d5f344b1eead1604f0f0c60d
Robert0101 <rob.tangled@gmail.com>
2022-08-24 01:41:16 -0300

    Borrar DELETE_RECIPE y SORT_BY_TITLE

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -26,11 +26,6 @@ const rootReducer = (state = initialState, action) => {
         ...state,
         recipeDetail: action.payload,
       };
-    case "DELETE_RECIPE":
-      return {
-        ...state,
-        recipes: state.recipes.filter((recip) => recip.id !== action.payload),
-      };
     case CREATE_RECIPE:
       return {
         ...state,
@@ -41,25 +36,6 @@ const rootReducer = (state = initialState, action) => {
         ...state,
         allFromDB: action.payload,
       };
-    //!------------------------------
-    case "SORT_BY_TITLE":
-      function compareTitle(a, b) {
-        // a should come before b in the sorted order
-        if (a.title < b.title) {
-          return -1;
-          // a should come after b in the sorted order
-        } else if (a.title > b.title) {
-          return 1;
-          // and and b are the same
-        } else {
-          return 0;
-        }
-      }
-      return {
-        ...state,
-        allFromDB: state.allFromDB.sort(compareTitle),
-      };
-    //!------------------------------
     case CLEAR_DETAIL:
       return {
         ...state,
