6b7ae6b3970bd5534c1b27c624fa627ba29d0125
Robert0101 <rob.tangled@gmail.com>
2022-08-24 21:40:02 -0300

    Purgar un poco

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -3,12 +3,7 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  //? El name es obligatorio? Seguramente sí.. pero no tiene el asterisco en el readme del PI.
   sequelize.define("diet", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -16,11 +16,12 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     summary: {
-      type: DataTypes.TEXT,
+      type: DataTypes.STRING,
       allowNull: false,
     },
     healthScore: {
       type: DataTypes.FLOAT,
+      allowNull: true,
       validate: {
         min: 0,
         max: 100,
@@ -29,18 +30,8 @@ module.exports = (sequelize) => {
     steps: {
       type: DataTypes.TEXT,
     },
-    diet: {
-      //Testeando este atributo:
+    image: {
       type: DataTypes.STRING,
-      allowNull: true,
     },
-    //Tengo que usar ese método tipo addModels() para asociar dietas a una receta? Entra como argumento a ese método un array de PKs. Y los PK tienen que ser los de las dietas!
-    //Le digo onda: si dietArray.length > 0, entonces pasá los PK de ese array como argumento del método que tengo que usar para asociar!! Creo que re va!!!!
-    // podría recibir un número que represente el id de la dieta en cuestión,
-    //Al crear las relaciones con los Recipe.hasMany(Diet);  Diet.hasMany(Recipe); se crean automáticamente unos métodos que son para relacionar las dos: set/addModels()
-    //Cuando me lleguen los datos por el req.body, chequeo si me enviaron las PK.
-    // o un array de números?
-    // después podría hacer un filtrado de todas las recetas que tienen en su propiedad diet ciertos números, y así filtrar las recetas según sus dietas. (Reviso el array y listo)
-    //podría recibir los nombres de las dietas en una frase separada por espacios?
   });
 };
