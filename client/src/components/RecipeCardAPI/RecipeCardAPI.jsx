2739be918c415f4e7cba4ce0334b76b8826b8e57
Robert0101 <rob.tangled@gmail.com>
2022-08-26 00:51:36 -0300

    Limpieza de código deprecado

--- client/src/components/RecipeCardAPI/RecipeCardAPI.jsx
+++ client/src/components/RecipeCardAPI/RecipeCardAPI.jsx
@@ -1,37 +1,12 @@
 import React from "react";
 import "./recipeCardAPI.css";
 import { Link } from "react-router-dom";
-// import { useDispatch, useSelector } from "react-redux";
-// import * as actions from "../../redux/actions";
-// Área donde se verá el listado de recetas. Deberá mostrar su:
-// Imagen
-// Nombre
-// Tipo de dieta (vegetariano, vegano, apto celíaco, etc)
 
 const RecipeCardAPI = (props) => {
-  // const dispatch = useDispatch();
-
-  function dietsToString(diets) {
-    let stringed = "";
-    diets?.forEach((element) => {
-      stringed += `${element} | `;
-    });
-    return stringed;
-  }
-
-  // function handleClickDetail(e) {
-  //   console.log("handleSubmitDetail invocado");
-  //   e.preventDefault();
-  //   dispatch(actions.getRecipeDetail(e.target.id));
-  //   console.log(`Soy el e.target.id: ${e.target.id}`);
-  //   console.log(e);
-  //   console.log(`Despaché el getRecipeDetail con id:  ${e.target.id}`);
-  // }
-
   return (
     <div key={props.id} className="recipeCardAPI">
       <img src={props.image} alt={`ilustration of recipe id: ${props.id}`} />{" "}
-      <h3 id={props.id}>
+      <h3>
         <Link to={`/home/recipes/${props.id}`}> {props.title} </Link>
       </h3>
       {/* <h3>Diets: {dietsToString(props.diets)} </h3> */}
