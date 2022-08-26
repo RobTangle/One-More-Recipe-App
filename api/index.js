d8ccde209696ff80c7e330a63adf0ec927bd7b33
Robert0101 <rob.tangled@gmail.com>
2022-08-26 00:47:19 -0300

    Cambiar force false a true porque si no se me agregar las dietas 100 veces

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require("./src/app.js");
 const { conn, Diet } = require("./src/db.js");
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   //! modifico el force! Estaba en force: true originalmente
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
