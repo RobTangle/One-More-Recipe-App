1e9ceee00e637d547b4b4c81fd3186cc2f34fbbb
Robert0101 <rob.tangled@gmail.com>
2022-08-27 20:04:44 -0300

    Borrar footer. Mejorar header. Mejorar css

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -6,19 +6,15 @@ const LandingPage = () => {
   return (
     <div className="container">
       <Link to="/home">
-        <div className="header" id="landing-page-header">
-          <h1 className="title">Welcome to one more food related web page!</h1>
-          <h3>
+        <div className="header-h1" id="landing-page-header">
+          <h1 className="title">Welcome to O.M.R.A!</h1>
+          <h3 className="presentational-text-h3">
             Here you will find not only the most awesome and delicious recipes,
             but also you'll be able to create your own ones!
           </h3>
           <h4>JUST CLICK HERE!</h4>
         </div>
       </Link>
-      <footer>
-        Foto de comida mediterranea creado por valeria_aksakova -
-        https://www.freepik.es/fotos/comida-mediterranea
-      </footer>
     </div>
   );
 };
