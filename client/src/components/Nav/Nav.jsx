ef78fa3e9a21ade4c19e082621be323dcb0f8b2b
Robert0101 <rob.tangled@gmail.com>
2022-08-28 13:24:48 -0300

    Agregar #nav-container a la div

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,11 +2,10 @@ import "./Nav.css";
 import { Link } from "react-router-dom";
 import React, { Component } from "react";
 
-//NO CAMBIEN EL CLASS COMPONENT A FUNCTIONAL COMPONENT PORQUE SINO LOS TEST NO VAN A CORRER!
 export default class Nav extends Component {
   render() {
     return (
-      <div className="nav">
+      <div className="nav" id="nav-container">
         <Link to="/">Landing page</Link>
         <Link to="/home">Home</Link>
         <Link to="/home/expRender">expRender</Link>
