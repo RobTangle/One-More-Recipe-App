0da3f4bb78a81edc251b5488483a136ebf3cb37b
Robert0101 <rob.tangled@gmail.com>
2022-08-29 18:24:33 -0300

    Cambiar código para que los botones se mapeen

--- client/src/components/FilterButtons/FilterButtons.jsx
+++ client/src/components/FilterButtons/FilterButtons.jsx
@@ -1,79 +1,84 @@
 import React from "react";
-
+import { useDispatch, useSelector } from "react-redux";
 import "./filterButtons.css";
+import bananaGif from "../../assets/470.gif";
+import * as actions from "../../redux/actions/index";
 
 const FilterButtons = ({ onFilterOptionChange, resetFilter }) => {
-  return (
-    <div
-      className="filter-buttons-container"
-      id="filter-buttons-container"
-      key={Math.random()}
-    >
-      <label htmlFor="label-filter-diets">
-        <span>Filter by diet</span>
-      </label>
+  //! experimentar con mapeo de botones:
+  // leo a todas las dietas:
+  const dietsInState = useSelector((state) => state.diets);
+  const dispatch = useDispatch();
 
-      <div className="filter-buttons">
-        <button
-          onClick={onFilterOptionChange}
-          value="gluten free"
-          id="glutenFree"
-        >
-          Gluten Free
-        </button>
-        <button value="dairyFree" id="dairyFree" onClick={onFilterOptionChange}>
-          Dairy Free
-        </button>
-        <button value="vegan" id="vegan" onClick={onFilterOptionChange}>
-          Vegan
-        </button>
-        <button
-          value="vegetarian"
-          id="vegetarian"
-          onClick={onFilterOptionChange}
-        >
-          Vegetarian
-        </button>
-        <button
-          value="lacto ovo vegetarian"
-          id="lacto-ovo-vegetarian"
-          onClick={onFilterOptionChange}
-        >
-          Lacto ovo vegetarian
-        </button>
-        <button
-          value="pescetarian"
-          id="pescetarian"
-          onClick={onFilterOptionChange}
-        >
-          Pescetarian
-        </button>
-        <button value="ketogenic" id="ketogenic" onClick={onFilterOptionChange}>
-          Ketogenic
-        </button>
-        <button value="paleo" id="paleo" onClick={onFilterOptionChange}>
-          Paleo
-        </button>
-        <button value="primal" id="primal" onClick={onFilterOptionChange}>
-          Primal
-        </button>
-        <button
-          value="lowFodmap"
-          id="low FODMAP"
-          onClick={onFilterOptionChange}
-        >
-          Low FODMAP
-        </button>
-        <button value="whole 30" id="whole30" onClick={onFilterOptionChange}>
-          Whole 30
-        </button>
+  function getDietsFromDB() {
+    dispatch(actions.setDietsToLoading());
+    dispatch(actions.getDiets());
+    console.log("dispatched getDiets()");
+  }
+
+  //! -----------------
+  return (
+    <>
+      {/* //! ------experimentar map btns: */}
+      <div
+        className="filter-buttons-component"
+        id="filter-buttons-component"
+        key={Math.random()}
+      >
+        <label htmlFor="label-filter-diets">
+          <span>Filter by diet</span>
+        </label>
+        {dietsInState?.loading ? (
+          <div className="loading-gif">
+            {/* <span>loading...</span> */}
+            <img src={bananaGif} alt="gif de carga" />
+          </div>
+        ) : null}
+
+        {dietsInState?.length > 0 ? (
+          <div className="filter-buttons-container">
+            <div className="filter-buttons">
+              {" "}
+              {dietsInState.map((diet) => (
+                <div key={diet.id}>
+                  <button
+                    key={Math.random()}
+                    type="button"
+                    name={diet.name}
+                    value={diet.name}
+                    onClick={onFilterOptionChange}
+                  >
+                    {" "}
+                    {diet.name}
+                  </button>
+                </div>
+              ))}
+            </div>
+            <div className="reset-filter">
+              <button className="reset-filter" onClick={resetFilter}>
+                Reset filter
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="no-diets">
+            <div>No diets loaded to the state</div>
+            <div className="get-diets-btn">
+              <button type="button" onClick={getDietsFromDB}>
+                Get diets
+              </button>
+            </div>
+          </div>
+        )}
+
+        {dietsInState?.error ? (
+          <div className="error-message">
+            Ups! There was an ERROR: "{dietsInState.error}"
+            <div>Try checking your internet connection</div>
+          </div>
+        ) : null}
       </div>
-      <div className="reset-filter">
-        <button className="reset-filter" onClick={resetFilter}>
-          Reset filter
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
