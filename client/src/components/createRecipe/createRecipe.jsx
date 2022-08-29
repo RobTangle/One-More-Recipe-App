69731e47ccfd3b966227a35d853604c57f2892f7
Robert0101 <rob.tangled@gmail.com>
2022-08-29 18:26:04 -0300

    Cambiar a de FormMapDiets a Form. Retocar visual

--- client/src/components/createRecipe/createRecipe.jsx
+++ client/src/components/createRecipe/createRecipe.jsx
@@ -3,15 +3,17 @@ import { useSelector } from "react-redux";
 import "./createRecipe.css";
 // import Form from "../CreateRecipeForm/Form";
 import bananaGif from "../../assets/470.gif";
+import cookingPot from "../../assets/icons8-cooking-pot-48.png";
+import zanyFace from "../../assets/icons8-zany-face-48.png";
 import OkSVG from "../../assets/icons8-ok.svg";
-import FormMapDiets from "../CreateRecipeForm/FormMapDiets";
+import Form from "../CreateRecipeForm/Form";
 
 const CreateRecipe = () => {
   const newRecipeState = useSelector((state) => state.newRecipe);
 
   return (
     <div className="createRecipe-container">
-      {newRecipeState.pure ? <FormMapDiets /> : null}
+      {newRecipeState.pure ? <Form /> : null}
       {newRecipeState.loading ? (
         <img src={bananaGif} alt="loading gif" />
       ) : null}
@@ -25,9 +27,11 @@ const CreateRecipe = () => {
         <div className="new-recipe-error-message">
           <h2>Ups! We stumbled over an error: "{newRecipeState.error}"</h2>
           <h4>
-            It seems that the recipe was lost. Please, check your internet
-            conection and try again
+            Please, check your internet conection and try again. <br /> Oh, and
+            your recipe was lost. So type it all again. Hope you had a back up!{" "}
           </h4>
+
+          <img src={zanyFace} alt="zani face" />
         </div>
       ) : null}
     </div>
