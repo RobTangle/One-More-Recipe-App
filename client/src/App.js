08efce1d441c09b56965eca170e52c02385e9a43
Robert0101 <rob.tangled@gmail.com>
2022-08-29 18:42:26 -0300

    retocar comments y comentar desuso

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { Route } from "react-router-dom";
-import RecipeDetail from "./components/RecipeDetail/RecipeDetail.jsx";
 import Nav from "./components/Nav/Nav.jsx";
 import createRecipe from "./components/createRecipe/CreateRecipe";
-import DBRecipes from "./components/DBRecipes/DBRecipes";
 import LandingPage from "./components/LandingPage/LandingPage";
 import SearchRecipes from "./components/SearchRecipes/SearchRecipes";
 import RenderRecipeCardsAPI from "./components/RenderRecipeCardsAPI/RenderRecipeCardsAPI";
 import RenderRecipeDetail from "./components/renderRecipeDetails/RenderRecipeDetails";
+// import DBRecipes from "./components/DBRecipes/DBRecipes";
 // import ExpRender from "./components/Paginacion/ExpRender";
 // import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
 
@@ -20,7 +19,6 @@ function App() {
       <Route exact path="/home" component={RenderRecipeCardsAPI} />
       <Route path="/home/recipes/:id" component={RenderRecipeDetail} />
       {/* <Route path="/home/expRender" component={ExpRender} /> */}
-
       <Route exact path="/home/createRecipe/" component={createRecipe} />
       {/* <Route exact path="/home/recipesInDataBase/" component={DBRecipes} /> */}
     </div>
