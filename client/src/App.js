747732a41f4526b05dc65c8d3f140bd845dca15a
Robert0101 <rob.tangled@gmail.com>
2022-08-29 18:26:30 -0300

    Adaptar para presentar

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,9 @@ import DBRecipes from "./components/DBRecipes/DBRecipes";
 import LandingPage from "./components/LandingPage/LandingPage";
 import SearchRecipes from "./components/SearchRecipes/SearchRecipes";
 import RenderRecipeCardsAPI from "./components/RenderRecipeCardsAPI/RenderRecipeCardsAPI";
-import ExpRender from "./components/Paginacion/ExpRender";
 import RenderRecipeDetail from "./components/renderRecipeDetails/RenderRecipeDetails";
-import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
+// import ExpRender from "./components/Paginacion/ExpRender";
+// import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
       <Route exact path="/home" component={SearchRecipes} />
       <Route exact path="/home" component={RenderRecipeCardsAPI} />
       <Route path="/home/recipes/:id" component={RenderRecipeDetail} />
-      <Route path="/home/expRender" component={ExpRender} />
+      {/* <Route path="/home/expRender" component={ExpRender} /> */}
 
       <Route exact path="/home/createRecipe/" component={createRecipe} />
       {/* <Route exact path="/home/recipesInDataBase/" component={DBRecipes} /> */}
