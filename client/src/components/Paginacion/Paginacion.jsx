8d7187f5c372c246892e292493beebe8af5115a0
Robert0101 <rob.tangled@gmail.com>
2022-08-30 23:36:40 -0300

    Modificar features del paginado

--- client/src/components/Paginacion/Paginacion.jsx
+++ client/src/components/Paginacion/Paginacion.jsx
@@ -1,42 +1,24 @@
 import React, { useState } from "react";
 import styles from "./paginacion.css";
 
-export const Paginacion = ({ page, setPage, maxPages }) => {
-  const [input, setInput] = useState(1);
+export const Paginacion = ({
+  page,
+  setPage,
+  maxPages,
+  localState,
+  quantity,
+}) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(localState?.length / quantity); i++) {
+    pageNumbers.push(i);
+  }
 
-  //---
-  // React.useEffect(() => {
-  //   console.log(`Soy el input: ${input}`);
-  // }, [input]);
-  //-----------------
   const nextPage = () => {
-    setInput(parseInt(input) + 1);
     setPage(parseInt(page) + 1);
   };
 
   const previousPage = () => {
-    setInput(parseInt(input) - 1);
     setPage(parseInt(page) - 1);
-  };
-
-  // const onKeyDown = (e) => {
-  //   if (e.keyCode === 13) {
-  //     setPage(parseInt(e.target.value));
-  //     if (
-  //       parseInt(e.target.value < 1) ||
-  //       parseInt(e.target.value) > Math.ceil(maxPages) ||
-  //       isNaN(parseInt(e.target.value))
-  //     ) {
-  //       setPage(1);
-  //       setInput(1);
-  //     } else {
-  //       setPage(parseInt(e.target.value));
-  //     }
-  //   }
-  // };
-
-  const onChange = (e) => {
-    setInput(e.target.value);
   };
 
   return (
@@ -59,14 +41,13 @@ export const Paginacion = ({ page, setPage, maxPages }) => {
           />
         </svg>
       </button>
-      <input
-        onChange={(e) => onChange(e)}
-        // onKeyDown={(e) => onKeyDown(e)}
-        name="page"
-        autoComplete="off"
-        value={page}
-      />
-      <p> of {Math.ceil(maxPages)} </p>
+      {pageNumbers &&
+        pageNumbers.map((n) => (
+          <div key={n}>
+            <button onClick={() => setPage(n)}> {n} </button>
+          </div>
+        ))}
+
       <button
         className="svg-right"
         disabled={page === Math.ceil(maxPages) || page > Math.ceil(maxPages)}
@@ -85,6 +66,12 @@ export const Paginacion = ({ page, setPage, maxPages }) => {
           />
         </svg>
       </button>
+      <div>
+        <p>
+          {" "}
+          {page} of {Math.ceil(maxPages)}{" "}
+        </p>
+      </div>
     </div>
   );
 };
