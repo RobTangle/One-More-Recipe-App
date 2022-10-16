060105894cf5a4580b1bcc87226142d3349cdf4c
Robert0101 <rob.tangled@gmail.com>
2022-10-16 14:51:26 -0300

    TS/Crear types para Recipe

--- api-TS/src/types/recipe-types.ts
+++ api-TS/src/types/recipe-types.ts
@@ -1,8 +1,50 @@
-export interface IRecipe {
-  id?: string;
+export interface IRecipeFromDB {
+  id?: string; //! SI CAMBIO ESTO DEBERÍA ASEGURARME EN TODO EL CÓDIGO QUE LO MANIPULO COMO UN NÚMERO.
   title: string;
   summary: string;
   healthScore?: number;
   steps?: string;
   image?: string;
+  diets?: string[];
+}
+
+export interface IRecipeFromApi {
+  id: number;
+  title: string;
+  vegetarian: boolean;
+  vegan: boolean;
+  glutenFree: boolean;
+  dairyFree: boolean;
+  lowFodmap: boolean;
+  healthScore: number;
+  summary: string;
+  dishTypes: string[];
+  image: string;
+  readyInMinutes: number;
+  analyzedInstruction: IAnalyzedInstruction[];
+  steps: IAnalyzedInstruction[];
+  // steps: axiData.analyzedInstructions?.[0]?.steps, //Atención a este ?.chain!
+  diets: string[];
+}
+
+export interface IRecipeFromDBPlusDietModel {
+  id?: string; //! SI CAMBIO ESTO DEBERÍA ASEGURARME EN TODO EL CÓDIGO QUE LO MANIPULO COMO UN NÚMERO.
+  title: string;
+  summary: string;
+  healthScore?: number;
+  steps?: string;
+  image?: string;
+  diets: any[];
+}
+
+export interface IAnalyzedInstruction {
+  name: string;
+  steps: IStep[];
+}
+
+export interface IStep {
+  number?: number;
+  step: string;
+  ingredients?: any[];
+  equipment?: any[];
 }
