b328ce016932a7906ee7b7b6e7d6acd8056f2844
Robert0101 <rob.tangled@gmail.com>
2022-10-18 17:02:59 -0300

    build/config/ Cambiar production

--- api-TS/build/src/config/config.js
+++ api-TS/build/src/config/config.js
@@ -2,28 +2,29 @@
 require("dotenv").config();
 const port = process.env.PORT || 3001;
 module.exports = {
-    server: {
-        port: port,
-    },
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: "RecipesTS",
-        host: process.env.DB_HOST,
-        dialect: "postgres",
-    },
-    test: {
-        username: "root",
-        password: null,
-        database: "database_test",
-        host: "127.0.0.1",
-        dialect: "mysql",
-    },
-    production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
-        dialect: "mysql",
-    },
+  server: {
+    port: port,
+  },
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "RecipesTS",
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    // username: "root",
+    // password: null,
+    // database: "database_production",
+    // host: "127.0.0.1",
+    // dialect: "mysql",
+  },
 };
