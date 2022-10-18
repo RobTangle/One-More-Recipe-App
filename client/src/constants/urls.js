ec4ee2119c26d5d538dbff25b14d08ac3f3fb478
Robert0101 <rob.tangled@gmail.com>
2022-10-18 17:42:59 -0300

    fix/Exportar const URL

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,3 +1,3 @@
-const URL_BACKEND_DEPLOY = "https://omra-backend.up.railway.app/";
-const URL_LOCALHOST = "http://localhost:3001/";
-const URL = URL_BACKEND_DEPLOY;
+export const URL_BACKEND_DEPLOY = "https://omra-backend.up.railway.app/";
+export const URL_LOCALHOST = "http://localhost:3001/";
+export const URL = URL_BACKEND_DEPLOY;
